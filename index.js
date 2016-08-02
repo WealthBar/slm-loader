26e6b8ecf709cdb72103927ec47745485c54eacf
Andrey Zharikov <andrey@zharikov.pro>
2016-08-02 14:16:57 +0300

    confirmed that loader still can be cacheable with proper dependency handling

--- index.js
+++ index.js
@@ -7,9 +7,7 @@ var markdown = require("slm-markdown");
 markdown.register(slm.template);
 
 module.exports = function(source) {
-  // force full rebuild when dependent files are changed
-  // - those referenced by extends('layout') and partial('block')
-  this.cacheable && this.cacheable(false);
+  this.cacheable && this.cacheable(true);
 
   var resolveRoot = this.options && this.options.resolve && this.options.resolve.root;
   var options = loaderUtils.getLoaderConfig(this, 'slmLoader') || {};
@@ -33,4 +31,4 @@ module.exports = function(source) {
   });
   
   return tmplFunc();
-}
+};
